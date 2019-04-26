ee09c2d3f93c4c4266e558ec84b799e45b4d2036
zhangyunp <zypolo91@gmail.com>
2019-04-26 13:16:38 +0800

    add log.js

--- template/lib/utils.js
+++ template/lib/utils.js
@@ -1,4 +1,11 @@
-const findServerConfig = ({ host = "", basePath = "/api/v1" }) => {
+const chalk = require('chalk')
+
+const log = console.log
+
+const findServerConfig = ({
+  host = "",
+  basePath = "/api/v1"
+}) => {
   let port;
   const parts = host.split(":");
   if (parts[1]) {
@@ -12,6 +19,15 @@ const findServerConfig = ({ host = "", basePath = "/api/v1" }) => {
   };
 };
 
+const error = (string) => void log(chalk.red(string))
+
+const success = (string) => void log(chalk.green(string))
+
+const warning = (string) => void log(chalk.yellow(string))
+
 module.exports = {
-  findServerConfig
+  findServerConfig,
+  error,
+  success,
+  warning
 };
