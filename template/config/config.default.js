3c244000c16541211f1130e54b5536c3b05e3319
zhangyunp <zypolo91@gmail.com>
2019-04-28 17:36:29 +0800

    add dir in specs

--- template/config/config.default.js
+++ template/config/config.default.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const appRoot = path.resolve(__dirname, "..");
 
-const docFilename = path.resolve(appRoot, "specs/swagger.json");
+const docFilename = path.resolve(appRoot, "specs/demo/swagger.json");
 
 const mockhost = 'localhost'
 
